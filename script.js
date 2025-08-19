2ec3f0c7683f5dc9ab84e3c040f9a40e22391ad7
Gabriel Horta <gabrielop847@gmail.com>
2025-08-19 10:44:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('header nav');
+
+    if (menuToggle && nav) {
+        menuToggle.addEventListener('click', function() {
+            nav.classList.toggle('active');
+        });
+    }
+
+});
